1061faf0457b1073077d377001d5d6a0675d618b
rodchar <rodchar@hotmail.com>
2012-08-25 01:03:34 -0400

    Add NUnit test project

---
+++ Solution1/ClassLibrary1.Test/ClassLibrary1.Test/Class1Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClassLibrary1.Test
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [SetUp]
+        protected void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            //arrange
+            decimal x = 1;
+            decimal y = 2;
+
+            //action
+            decimal expected = 3;
+            decimal actual = Class1.Add(x,y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TearDown]
+        protected void Teardown(){
+        }
+    }
+}
