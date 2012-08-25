9f23aa0e39a8da7dc088897e9fbc1ac9de0cefa1
rodchar <rodchar@hotmail.com>
2012-08-24 23:34:30 -0400

    Initial checkin 8/23/2012

---
+++ Solution1/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+    }
+}
