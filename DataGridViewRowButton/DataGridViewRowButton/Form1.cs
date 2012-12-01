af95a2ffcf9d7d9a417007f9834ca024d026a1b4
rodchar <rodchar@hotmail.com>
2012-12-01 10:38:54 -0500

    Add Button to C# DataGridView http://tinyurl.com/blsj6rd

---
+++ DataGridViewRowButton/DataGridViewRowButton/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+namespace DataGridViewRowButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Add Button to C# DataGridView 
+            //http://tinyurl.com/blsj6rd
+
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Product ID";
+            dataGridView1.Columns[1].Name = "Product Name";
+            dataGridView1.Columns[2].Name = "Product Price";
+
+            string[] row = new string[] { "1", "Product 1", "1000" };
+            dataGridView1.Rows.Add(row);
+            row = new string[] { "2", "Product 2", "2000" };
+            dataGridView1.Rows.Add(row);
+            row = new string[] { "3", "Product 3", "3000" };
+            dataGridView1.Rows.Add(row);
+            row = new string[] { "4", "Product 4", "4000" };
+            dataGridView1.Rows.Add(row);
+
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            dataGridView1.Columns.Add(btn);
+            btn.HeaderText = "Click Data";
+            btn.Text = "Click Here";
+            btn.Name = "btn";
+            btn.UseColumnTextForButtonValue = true;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3)
+            {
+                MessageBox.Show((e.RowIndex + 1) + "  Row  " + (e.ColumnIndex + 1) + "  Column button clicked ");
+            }
+        }
+    }
+}
