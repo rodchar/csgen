5025decef1a20fbd123bb90ef1fb4c51dd23ead6
rodchar <rodchar@hotmail.com>
2013-02-10 23:40:07 -0500

    Master Detail CRUD example. There's 2 solutions. One for building the dataset since c# express doesn't let you use sql server and web developer does.

---
+++ Master Details CRUD/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(447, 19);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(217, 98);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 172);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(474, 205);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(214, 71);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(0, 13);
+            this.lblId.TabIndex = 5;
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(35, 41);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnAddProduct.TabIndex = 6;
+            this.btnAddProduct.Text = "Add Product";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(35, 70);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(35, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(341, 21);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(100, 20);
+            this.tbFind.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 389);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.TextBox tbFind;
+    }
+}
+
---
+++ Master Details CRUD/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        /*
+        * Prerequisite:
+        * Rewards table
+        * -------------
+        * Id, OrderDate
+        * 
+        * RewardRequirements table
+        * ---------------------
+        * Id, OrderId, ProductId, CategoryId, Quantity, ProductName*
+        * 
+        * ProductName is local to DataSet and doesn't exist in the table.
+        * It is there for the DataGridView Order Details table.
+        * 
+        * Resources:
+        * http://stackoverflow.com/q/508293/139698
+
+        * 
+        * */
+
+        DataSet1 _ds;
+        DataSet1TableAdapters.TableAdapterManager _dm;
+        int _orderId;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _dm = new WindowsFormsApplication1.DataSet1TableAdapters.TableAdapterManager();
+            _dm.RewardsTableAdapter = new WindowsFormsApplication1.DataSet1TableAdapters.RewardsTableAdapter();
+            _dm.RewardRequirementsTableAdapter = new WindowsFormsApplication1.DataSet1TableAdapters.RewardRequirementsTableAdapter();
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            UnBindFormData();
+
+            if (_ds == null)
+            {
+                _ds = new DataSet1();
+                AddNewOrder();
+            }
+
+            Button b = sender as Button;
+
+            switch (b.Name)
+            {
+                case "btnAddProduct":
+                    AddNewProduct();
+                    break;
+                case "btnSave":
+                    SaveOrder();
+                    BindFormData();
+                    break;
+                case "btnFind":
+                    LoadOrder();
+                    BindFormData();
+                    break;
+                case "btnNew":
+                    NewOrder();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void AddNewOrder()
+        {
+            DataSet1.RewardsRow drRewards = _ds.Rewards.NewRewardsRow();
+            _orderId = drRewards.Id;
+            drRewards.Name = tbName.Text;
+            _ds.Rewards.Rows.Add(drRewards);
+        }
+
+        private void AddNewProduct()
+        {
+            DataSet1.RewardRequirementsRow drRewardRequirements = _ds.RewardRequirements.NewRewardRequirementsRow();
+            drRewardRequirements.RewardId = _orderId == 0 ? _orderId : _orderId;
+            drRewardRequirements.Quantity = 9;
+            _ds.RewardRequirements.Rows.Add(drRewardRequirements);
+
+            dataGridView1.DataSource = _ds.RewardRequirements;
+        }
+
+        private void NewOrder()
+        {
+            _ds = null;
+            lblId.Text = string.Empty;
+            tbName.Text = string.Empty;
+            dataGridView1.DataSource = null;
+        }
+
+        private void LoadOrder()
+        {
+            //Todo: Remove. For testing purposes
+            int orderId = 0;
+            int.TryParse(tbFind.Text, out orderId);
+            _orderId = orderId;
+
+            _ds = new DataSet1();
+
+            _ds.Rewards.Merge(_dm.RewardsTableAdapter.GetDataById(orderId));
+            _ds.RewardRequirements.Merge(_dm.RewardRequirementsTableAdapter.GetDataByRewardId(orderId));
+            dataGridView1.DataSource = _ds.RewardRequirements;
+        }
+
+        private void SaveOrder()
+        {
+            if (_ds != null)
+            {
+                int result = 0;
+                //So _ds gets refreshed after update for free
+                result = _dm.UpdateAll(_ds);
+                //As a result of the refresh why not bind it, right?
+                //See switch statement
+                //_ds = null;
+                //_orderId = 0;
+            }
+        }
+
+        private void BindFormData()
+        {
+            lblId.DataBindings.Add("Text", _ds.Rewards, "Id");
+            tbName.DataBindings.Add("Text", _ds.Rewards, "Name");
+        }
+
+        private void UnBindFormData()
+        {
+            lblId.DataBindings.Clear();
+            tbName.DataBindings.Clear();
+        }
+    }
+}
