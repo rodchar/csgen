8c16af1ad687c343704384df454d0342361025c9
Rodney Santiago <rodchar@hotmail.com>
2013-02-23 20:10:43 -0500

    bp tab prototype screens
    wanted to get a diff of generated code to extract into my own method.
    This way I don't have to drag an instance out to designer.

---
+++ BPScreenPrototype1/BPScreenPrototype1/ucDataGridViewHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ucDataGridViewHelper : UserControl
+    {
+        public ucDataGridViewHelper()
+        {
+            InitializeComponent();
+        }
+
+        public DataSet DataSource { get; set; }
+
+        public void DataBind()
+        {
+            dataGridView1.DataSource = this.DataSource;
+        }
+    }
+}
---
+++ BPScreenPrototype1/BPScreenPrototype1/ucRewards.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ucRewards
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucDataGridViewHelper3 = new WindowsFormsApplication1.ucDataGridViewHelper();
+            this.ucDataGridViewHelper2 = new WindowsFormsApplication1.ucDataGridViewHelper();
+            this.ucDataGridViewHelper1 = new WindowsFormsApplication1.ucDataGridViewHelper();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ucDataGridViewHelper3
+            // 
+            this.ucDataGridViewHelper3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ucDataGridViewHelper3.Location = new System.Drawing.Point(208, 200);
+            this.ucDataGridViewHelper3.Name = "ucDataGridViewHelper3";
+            this.ucDataGridViewHelper3.Size = new System.Drawing.Size(300, 148);
+            this.ucDataGridViewHelper3.TabIndex = 2;
+            // 
+            // ucDataGridViewHelper2
+            // 
+            this.ucDataGridViewHelper2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ucDataGridViewHelper2.Location = new System.Drawing.Point(22, 200);
+            this.ucDataGridViewHelper2.Name = "ucDataGridViewHelper2";
+            this.ucDataGridViewHelper2.Size = new System.Drawing.Size(148, 148);
+            this.ucDataGridViewHelper2.TabIndex = 1;
+            // 
+            // ucDataGridViewHelper1
+            // 
+            this.ucDataGridViewHelper1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ucDataGridViewHelper1.Location = new System.Drawing.Point(22, 30);
+            this.ucDataGridViewHelper1.Name = "ucDataGridViewHelper1";
+            this.ucDataGridViewHelper1.Size = new System.Drawing.Size(148, 148);
+            this.ucDataGridViewHelper1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(352, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // ucRewards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ucDataGridViewHelper3);
+            this.Controls.Add(this.ucDataGridViewHelper2);
+            this.Controls.Add(this.ucDataGridViewHelper1);
+            this.Name = "ucRewards";
+            this.Size = new System.Drawing.Size(542, 374);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public ucDataGridViewHelper ucDataGridViewHelper1;
+        public ucDataGridViewHelper ucDataGridViewHelper2;
+        public ucDataGridViewHelper ucDataGridViewHelper3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
